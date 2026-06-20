21285d41620ae6eaf3ac85ee412dc172c58e4823
Core-SebastianParejo <sparejo@coreeb.com>
2026-06-20 11:02:21 -0500

    feat: creation of schema prisma, model, db connection

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,3 @@
-// Leemos el puerto desde las variables de entorno, convirtiéndolo a número.
-// Si no existe, usamos el 3000 como respaldo (fallback).
 export const env = {
   PORT: Number(process.env.PORT) || 3000,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from './app.js';
 import { env } from './config/env.js';
 
+
+
 app.listen(env.PORT, () => {
   console.log(`App Listening on http://Localhost:${env.PORT}/api`);
 });
